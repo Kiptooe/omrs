65a898cdb81a0b5e9e2e9258330362368312da4e
Emmanuel-Kip <84289692+Emmanuel-Kip@users.noreply.github.com>
2022-07-06 18:02:18 +0300

    Added Patientroles controller,pastvisit view and patientvisit model

---
+++ app/Controllers/Patientroles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Patientroles extends BaseController
+{
+    public function index()
+    {
+        return view('navigation');
+    }
+}
---
+++ app/Views/pastvisit.php
@@ -0,0 +1,6 @@
+<html>
+    <title> Past visit</title>
+    <body>
+        <p>Shows patient past visit</p>
+    </body>
+</html>
