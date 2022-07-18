1b226ade564119a83763d38289f5ad7bea741844
Emmanuel-Kip <84289692+Emmanuel-Kip@users.noreply.github.com>
2022-07-18 12:27:27 +0300

    Modified patient vitals to show interpreted blood pressure

--- app/Models/tbl_vitals.php
+++ app/Models/tbl_vitals.php
@@ -8,7 +8,7 @@ class tbl_vitals extends Model{
     
 protected $table = 'tbl_vitals';
 protected $primaryKey = 'vital_id';
-protected $allowedfields = ['patient_id','visit_id','date','blood_pressure','temperature','weight','role_id','pulse_rate','is_deleted'];
+protected $allowedfields = ['patient_id','visit_id','date','systolic_pressure','diastolic_pressure','temperature','weight','role_id','pulse_rate','is_deleted'];
 
 public function fetchVitals($id){
 
@@ -17,7 +17,19 @@ public function fetchVitals($id){
                  ->orderBy('visit_id','DESC')
                  ->first();
 
-    $vitals = array('Blood Pressure'=>$vitals['blood_pressure'],
+    if($vitals['systolic_pressure']<90 && $vitals['diastolic_pressure']<60){
+        $vitals['systolic_pressure'] = 'Low';
+    }elseif ($vitals['systolic_pressure']>=140 && $vitals['diastolic_pressure']<90) {
+        $vitals['systolic_pressure'] = 'Isolated systolic hypertension';
+    }elseif ($vitals['systolic_pressure']>90 && $vitals['systolic_pressure']<120 && $vitals['diastolic_pressure']>60 && $vitals['diastolic_pressure']<80){
+        $vitals['systolic_pressure'] = 'Optimal';
+    }elseif ($vitals['systolic_pressure']>=120 && $vitals['systolic_pressure']<140 && $vitals['diastolic_pressure']>=80 && $vitals['diastolic_pressure']<90){
+        $vitals['systolic_pressure'] = 'Normal';
+    }elseif ($vitals['systolic_pressure']>=140 && $vitals['diastolic_pressure']>=90) {
+        $vitals['systolic_pressure'] = 'Hypertensive';
+    }
+
+    $vitals = array('Blood Pressure'=>$vitals['systolic_pressure'],
                     'Temperature'=>$vitals['temperature'],
                     'Weight'=>$vitals['weight'],
                     'Pulse Rate'=>$vitals['pulse_rate']
